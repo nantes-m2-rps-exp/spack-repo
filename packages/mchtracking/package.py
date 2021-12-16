eb6638300ba58928414ffc650aeb95dabfe5ce20
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-12-16 16:00:16 +0100

    initial commit

---
+++ packages/mchtracking/package.py
@@ -0,0 +1,42 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+import os
+
+class MchTracking(CMakePackage):
+    """Lite version of Alice MCH Tracking library and its Python bindings."""
+
+    homepage = "https://www.example.com"
+    git      = "https://github.com/nantes-m2-rps-exp/mchtracking.git"
+    url      = "https://github.com/nantes-m2-rps-exp/mchtracking/archive/refs/tags/v0.0.1.tar.gz"
+    
+    extends('python')
+    generator = "Ninja"
+
+    version('dev',branch='dev')
+    version('0.0.1', sha256='0e9fb4f778417a62170888d558426107f30c64d64634e3799e38f37278c5998d')
+
+    depends_on('boost')
+    depends_on('fairlogger')
+    depends_on('fmt')
+    depends_on('ninja',type='build')
+    depends_on('py-pybind11',type='build')
+    depends_on('py-pytest',type='build')
+    depends_on('root')
+
+    def cmake_args(self):
+        args = []
+        args.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS",True))
+        return args
+
+    def setup_build_environment(self,env):
+        env.set('O2_ROOT',os.path.join(self.build_directory,'stage'))
+        env.prepend_path('PYTHONPATH',os.path.join(self.build_directory,'stage/lib'))
+
+    def setup_run_environment(self,env):
+        env.set('O2_ROOT',self.prefix)
+        env.append_path('ROOT_INCLUDE_PATH',os.path.join(self.prefix,'include'))
---
+++ packages/py-vector/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+class PyVector(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url = "https://files.pythonhosted.org/packages/db/06/1918bb3014c6537ce36a9af7e7d3957a368216dd06a82bee5cf5ca1acee9/vector-0.8.4.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('0.8.4', sha256='ef97bfec0263766edbb74c290401f89921f8d11ae9e4a0ffd904ae40674f1239')
+
+    # FIXME: Add dependencies if required. Only add the python dependency
+    # if you need specific versions. A generic python dependency is
+    # added implicity by the PythonPackage class.
+    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
+    # depends_on('py-setuptools', type='build')
+    depends_on('py-numpy')
+    depends_on('py-packaging')
+    depends_on('py-importlib-metadata')
+
+    def build_args(self, spec, prefix):
+        # FIXME: Add arguments other than --prefix
+        # FIXME: If not needed delete this function
+        args = []
+        return args
