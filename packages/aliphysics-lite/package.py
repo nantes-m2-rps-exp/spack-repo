ae39d7187286a748e64ce2c3fbc1227781ef7423
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2021-12-28 21:13:55 +0100

    aliphysics: bump to 1.1.1

--- packages/aliphysics-lite/package.py
+++ packages/aliphysics-lite/package.py
@@ -13,6 +13,7 @@ class AliphysicsLite(CMakePackage):
     url      = "https://github.com/nantes-m2-rps-exp/aliphysics-lite/archive/refs/tags/1.1.0.tar.gz"
 
     version("master",branch="master")
+    version('1.1.1', sha256='eb9d06fe33c3243ee4f02d61af58b5b7c4d82ba75354ce747f833de2051bcde0')
     version('1.1.0', sha256='028b92cf033ff484fd70812f120cca92fba3ed63ed7db2e10cb3050b971b3590')
     version('1.0.0', sha256='e57a14c0b4e1bba93e11f5a8d774d2050ab9b83625880882615f6206bdee7604')
 
